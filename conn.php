189ca03cfaaeda7ab9eaae8bef6db76c488ca7af
DevOnTheGo <sotohernandezbrian5@gmail.com>
2025-07-10 15:54:51 +0000

    ENV configuration

---
+++ conn.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['HOST'];
+$db   = $_ENV['BD'];
+$user = $_ENV['USER'];
+$pass = $_ENV['PASS'];
+
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Right";
+    // Conexión lista
+} catch (PDOException $e) {
+    echo "Left";
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
